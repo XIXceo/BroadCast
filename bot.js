7fe2c4da717af0aa3672b6b11dc70f078c135ea4
QHOIZEUATHQERM <58605670+QHOIZEUATHQERM@users.noreply.github.com>
2020-01-11 19:02:48 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,7 +56,7 @@ client.on('ready',  () => {
   client.on('message', message => {
     if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('-adminbc')){
+if (message.content.startsWith('-bcPRO')){
 if(!message.author.id === '476185102922285066') return;
 message.channel.sendMessage('**جار ارسال الرسالة** |:white_check_mark:')
 client.users.forEach(m =>{
@@ -67,7 +67,7 @@ m.sendMessage(args)
 
   client.on('message', msg => {
     if(msg.content === '-help')
-    msg.reply('Check Your DM :white_check_mark:')
+    msg.reply('**لقد تلقيت الرسالة في الخاص** :white_check_mark:')
   });
   
   
